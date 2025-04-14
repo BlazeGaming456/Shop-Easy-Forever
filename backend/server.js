0a0e94e6c8273463617caa16734fa61b3da36fb4
unknown <ajincraju@gmail.com>
2025-04-14 14:05:03 +0530

    Modified the Urls

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000;
 // CORS Middleware must come FIRST
 const corsOptions = {
   origin: [
-    'https://shop-easy-forever-frontend-8nsuresmj-blazegaming456s-projects.vercel.app',
+    'https://shop-easy-forever-frontend-c700pp3sd-blazegaming456s-projects.vercel.app/',
     'http://localhost:3000',
     'http://localhost:5173'
   ],
